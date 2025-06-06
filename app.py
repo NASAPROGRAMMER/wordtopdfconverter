56ad21b1a8d571b3c32c0b96f6888aba12248022
MasR(idk) <rangkay57@gmail.com>
2025-06-06 22:33:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def convert():
         return f"Unexpected error: {str(e)}", 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
